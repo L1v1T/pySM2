83e6a770842ec13f02ffe49a1c926d65d5516b4b
Tris007 <37336792+Tris007@users.noreply.github.com>
2019-02-20 15:15:03 +0800

    Add files via upload

---
+++ Point.py
@@ -0,0 +1,22 @@
+class Point(object):
+    def __init__(self,x=0,y=0):
+        self._x=x
+        self._y=y
+    @property
+    def x(self):
+        return self._x
+    @x.setter
+    def x(self, value):
+        self._x = value
+    @property
+    def y(self):
+        return self._y
+    @y.setter
+    def y(self, value):
+        self._y = value
+    def distance(p1,p2):
+        return((p1.x-p2.x)**2+(p1.y-p2.y)**2)**0.5
+    def isnearby(self,p):
+        return((self.x-p2.x)**2+(self.y-p2.y)**2)**0.5
+    def __str__(self):
+        return '('+str(self.x)+','+str(self.y)+')'
