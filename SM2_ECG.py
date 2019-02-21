be57553c57ea21d79ed97f74427926f6dd0913c4
L1v1T <Chenyq@mail.ustc.edu.cn>
2019-02-21 17:13:57 +0800

    Update ECG point methods

--- SM2_ECG.py
+++ SM2_ECG.py
@@ -5,8 +5,49 @@ import math
 from polynomial import *
 from Point import *
 
-# 3.1 有限域计算
-# 有限域加法
+# 有限域加法单位元 #
+def field_ele_zero():
+    q = config.get_q()
+    # q 为奇素数
+    if isPrime(q) and q > 2:
+        return 0
+    # q 为 2 的幂
+    elif is_Power_of_two(q):
+        m = int(math.log2(q))
+        zero = '0b'
+        for i in range(0, m):
+            zero += '0'
+        return zero
+    else:
+        print("*** ERROR: 模数q不是奇素数或者2的幂 *** function: field_ele_zero ***")
+        return -1
+### test field_ele_zero ###
+#config.set_q(16)
+#print(field_ele_zero())
+
+# 有限域乘法单位元 #
+def field_ele_one():
+    q = config.get_q()
+    # q 为奇素数
+    if isPrime(q) and q > 2:
+        return 1
+    # q 为 2 的幂
+    elif is_Power_of_two(q):
+        m = int(math.log2(q))
+        one = '0b'
+        for i in range(0, m - 1):
+            one += '0'
+        one += '1'
+        return one
+    else:
+        print("*** ERROR: 模数q不是奇素数或者2的幂 *** function: field_ele_one ***")
+        return -1
+### test field_ele_one ###
+#config.set_q(16)
+#print(field_ele_one())
+
+# 3.1 有限域计算 #
+# 有限域加法 #
 '''
 input: 域元素 a 和 b
 output: 域元素 (a+b)
@@ -45,7 +86,47 @@ def field_ele_add(a, b):
 #config.set_q(1024)
 #print(field_ele_add('0b1010101010', '0b1111111100'))
 
-# 有限域乘法
+# 有限域加法逆元 #
+'''
+input: 域元素 a
+output: a 的逆元素
+'''
+def field_ele_inverse_add(a):
+    q = config.get_q()
+    # q 为奇素数
+    if isPrime(q) and q > 2:
+        if not (a >= 0 and a <= q-1):
+            print("*** ERROR: a不是域中元素 *** function: field_ele_inverse_add ***")
+            return -1
+        else:
+            return(q - a)
+    # q 为 2 的幂
+    elif is_Power_of_two(q):
+        m = math.log2(q)
+        if (len(a)-2) > m:
+            print("*** ERROR: 参数不是二元扩域元素 *** function: field_ele_inverse_add ***")
+            return -1
+        else:
+            return a
+    else:
+        print("*** ERROR: 模数q不是奇素数或2的幂 *** function: field_ele_inverse_add ***")
+        return -1
+### test field_ele_inverse_add ###
+#config.set_q(23)
+#print(field_ele_inverse_add(8))
+#config.set_q(16)
+#config.set_fx('0b10011')
+#print(field_ele_inverse_add('0b0101'))
+
+# 有限域减法 #
+'''
+input: 被减元素 a 和减元素 b
+output: 域元素 (a-b)
+'''
+def field_ele_sub(a, b):
+    return field_ele_add(a, field_ele_inverse_add(b))
+
+# 有限域乘法 #
 '''
 input: 域元素 a 和 b
 output: 域元素 (a*b)
@@ -56,10 +137,10 @@ def field_ele_times(a, b):
     q = config.get_q()
     # q 为奇素数
     if isPrime(q) and q > 2:
-        if not (a >= 0 and a<= q-1):
+        if not (a >= 0 and a <= q-1):
             print("*** ERROR: a不是域中元素 *** function: field_ele_times ***")
             return -1
-        elif not (b >=0 and b<= q-1):
+        elif not (b >= 0 and b <= q-1):
             print("*** ERROR: b不是域中元素 *** function: field_ele_times ***")
             return -1
         else:
@@ -79,82 +160,115 @@ def field_ele_times(a, b):
 ### test field_ele_time ###
 #config.set_q(997)
 #print(field_ele_times(56, 46))
-#config.set_q(32)
-#config.set_fx('0b100101')
-#print(field_ele_times('0b111', '0b1101'))
+#config.set_q(16)
+#config.set_fx('0b10011')
+#print(field_ele_times('0b0100', '0b1110'))
 
-# 有限域除法
+# 有限域幂运算 #
+'''
+iuput: 域元素 g 和 幂次 a
+output: 域元素 g**a
+'''
+def field_ele_g_pow_a(g, a):
+    #print("--- 有限域 幂运算 ---")
+
+    q = config.get_q()
+    # q 为奇素数
+    if isPrime(q) and q > 2:
+        if not (g >= 0 and g <= q-1):
+            print("*** ERROR: a不是域中元素 *** function: field_ele_g_pow_a ***")
+            return -1
+        else:
+            e = a % (q - 1)
+            if e == 0:
+                return 1
+            r = int(math.log2(e))# + 1 - 1
+            x = g
+            for i in range(0, r):
+                x = field_ele_times(x, x)
+                if (e & (1 << (r - 1 - i))) == (1 << (r - 1 - i)):
+                    x = field_ele_times(x, g)
+            return x
+    # q 为 2 的幂
+    elif is_Power_of_two(q):
+        m = math.log2(q)
+        if (len(g)-2) > m:
+            print("*** ERROR: 参数不是二元扩域元素 *** function: field_ele_g_pow_a ***")
+            return -1
+        else:
+            e = a % (q -1)
+            if e == 0:
+                return polynomial_one()
+            r = int(math.log2(e))# + 1 - 1
+            x = g
+            for i in range(0, r):
+                x = field_ele_times(x, x)
+                if (e & (1 << (r - 1 - i))) == (1 << (r - 1 - i)):
+                    x = field_ele_times(x, g)
+            return x
+    else:
+        print("*** ERROR: 模数q不是奇素数或2的幂 *** function: field_ele_g_pow_a ***")
+        return -1
+### test field_ele_g_pow_a ###
+#config.set_q(23)
+#print(field_ele_g_pow_a(8, 2))
+#config.set_q(16)
+#config.set_fx('0b10011')
+#print(field_ele_g_pow_a('0b0010', 9))
+
+# 有限域逆元素 #
+'''
+input: 元素 a
+output: 元素 a 的逆元素
+'''
+def field_ele_inverse_times(a):
+    q = config.get_q()
+    # q 为奇素数
+    if isPrime(q) and q > 2:
+        if not (a >= 0 and a <= q-1):
+            print("*** ERROR: a不是域中元素 *** function: field_ele_inverse_times ***")
+            return -1
+        else:
+            return field_ele_g_pow_a(a, config.get_q() - 2)
+    # q 为 2 的幂
+    elif is_Power_of_two(q):
+        m = math.log2(q)
+        if (len(a)-2) > m:
+            print("*** ERROR: 参数不是二元扩域元素 *** function: field_ele_inverse_times ***")
+            return -1
+        else:
+            return field_ele_g_pow_a(a, config.get_q() - 2)
+    else:
+        print("*** ERROR: 模数q不是奇素数或2的幂 *** function: field_ele_inverse_times ***")
+        return -1
+### test field_ele_inverse_times ###
+#config.set_q(23)
+#print(field_ele_inverse_times(8))
+#config.set_q(16)
+#config.set_fx('0b10011')
+#print(field_ele_inverse_times('0b0010'))
+
+# 有限域除法 #
 '''
 input: 被除数 a 和除数 b
 output: 除法所得商
 '''
 def field_ele_a_devide_b(a, b):
     #print("--- 有限域 除法 ---")
-
-    q = config.get_q()
-    # q 为奇素数
-    if isPrime(q) and q > 2:
-        if not (a >= 0 and a <= q-1):
-            print("*** ERROR: a不是域中元素 *** function: field_ele_a_devide_b ***")
-            return -1
-        elif not (b >= 0 and b <= q-1):
-            print("*** ERROR: b不是域中元素 *** function: field_ele_a_devide_b ***")
-            return -1
-        else:
-            return int(a / b)
-    # q 为 2 的幂
-    elif is_Power_of_two(q):
-        m = math.log2(q)
-        if (len(a)-2) > m or (len(b)-2) >m:
-            print("*** ERROR: 参数不是二元扩域元素 *** function: field_ele_a_devide_b ***")
-            return -1
-        else:
-            result_bits = polynomial_a_mod_b(polynomial_times(a, b), config.get_fx())
-            return bytes_to_ele(q, bits_to_bytes(result_bits))
-    else:
-        print("*** ERROR: 模数q不是奇素数或2的幂 *** function: field_ele_a_devide_b ***")
-        return -1
-
-# 有限域幂运算
-'''
-iuput: 域元素 a 和 幂次 x
-output: 域元素 a**x
-'''
-def field_ele_a_pow_x(a, x):
-    #print("--- 有限域 幂运算 ---")
-
-    q = config.get_q()
-    # q 为奇素数
-    if isPrime(q) and q > 2:
-        if not (a >= 0 and a<= q-1):
-            print("*** ERROR: a不是域中元素 *** function: field_ele_a_pow_x ***")
-            return -1
-        else:
-            return((a**x) % q)
-    # q 为 2 的幂
-    elif is_Power_of_two(q):
-        m = math.log2(q)
-        if (len(a)-2) > m:
-            print("*** ERROR: 参数不是二元扩域元素 *** function: field_ele_a_pow_x ***")
-            return -1
-        else:
-            result_bits = polynomial_one()
-            if x != 0:
-                for i in range(1, x + 1):
-                    result_bits = polynomial_times(result_bits, a)
-                result_bits = polynomial_a_mod_b(result_bits, config.get_fx())
-            return bytes_to_ele(q, bits_to_bytes(result_bits))
-    else:
-        print("*** ERROR: 模数q不是奇素数或2的幂 *** function: field_ele_a_pow_x ***")
-        return -1
-### test field_ele_a_pow_x ###
+    return field_ele_times(a, field_ele_inverse_times(b))
+### test field_ele_a_devide_b ###
 #config.set_q(23)
-#print(field_ele_a_pow_x(7, 4))
+#print(field_ele_a_devide_b(3, 8))
 #config.set_q(16)
 #config.set_fx('0b10011')
-#print(field_ele_a_pow_x('0b0010', 9))
+#print(field_ele_a_devide_b('0b1001', '0b0101'))
 
 # 3.2.3 椭圆曲线群 #
+
+# 椭圆曲线无穷远点 #
+def ECG_ele_zero():
+    return Point(0, 0)
+
 # 椭圆曲线元素判断 #
 # 元素为零 #
 def ECG_ele_is_zero(p):
@@ -203,16 +317,21 @@ def ECG_ele_add(p1, p2):
             return Point(0, 0)
         elif ECG_ele_equal(p1, p2):
             #lam = (3 * (p1.x**2) + config.get_a()) / (2 * p1.y)
-            t1 = field_ele_add(field_ele_times(3, field_ele_a_pow_x(p1.x, 2)), config.get_a())
+            t1 = field_ele_add(field_ele_times(3, field_ele_g_pow_a(p1.x, 2)), config.get_a())
             t2 = field_ele_times(2, p1.y)
             lam = field_ele_a_devide_b(t1, t2)
-            x = lam**2 - 2 * p1.x
-            y = lam * (p1.x - x) - p1.y
+            #x = lam**2 - 2 * p1.x
+            x = field_ele_sub(field_ele_g_pow_a(lam, 2), field_ele_times(2, p1.x))
+            #y = lam * (p1.x - x) - p1.y
+            y = field_ele_sub(field_ele_times(lam, field_ele_sub(p1.x, x)), p1.y)
             return Point(x, y)
         else:
-            lam = (p2.y - p1.y) / (p2.x - p1.x)
-            x = lam * lam - p1.x - p2.x
-            y = lam * (p1.x - x) - p1.y
+            #lam = (p2.y - p1.y) / (p2.x - p1.x)
+            lam = field_ele_a_devide_b(field_ele_sub(p2.y, p1.y), field_ele_sub(p2.x, p1.x))
+            #x = lam * lam - p1.x - p2.x
+            x = field_ele_sub(field_ele_sub(field_ele_g_pow_a(lam, 2), p1.x), p2.x)
+            #y = lam * (p1.x - x) - p1.y
+            y = field_ele_sub(field_ele_times(lam, field_ele_sub(p1.x, x)), p1.y)
             return Point(x, y)
 
     # F2^m 上的椭圆曲线
@@ -224,14 +343,26 @@ def ECG_ele_add(p1, p2):
         elif ECG_is_inverse_ele(p1, p2):
             return Point(0, 0)
         elif ECG_ele_equal(p1, p2):
-            lam = p1.x + (p1.y / p1.x)
-            x = lam**2 + lam + config.get_a()
-            y = p1.x**2 + (lam + 1) * x
+            #lam = p1.x + (p1.y / p1.x)
+            lam = field_ele_add(p1.x, field_ele_a_devide_b(p1.y, p1.x))
+            #x = lam**2 + lam + config.get_a()
+            x = field_ele_add(field_ele_add(field_ele_g_pow_a(lam, 2), lam), config.get_a())
+            #y = p1.x**2 + (lam + 1) * x
+            y = field_ele_add(field_ele_g_pow_a(p1.x, 2), \
+                field_ele_times(field_ele_add(lam, field_ele_one()), x))
             return Point(x, y)
         else:
-            lam = (p1.y + p2.y) / (p1.x + p2.x)
-            x = lam**2 + lam + p1.x + p2.x + config.get_a()
-            y = lam * (p1.x + x) + x + p1.y
+            #lam = (p1.y + p2.y) / (p1.x + p2.x)
+            lam = field_ele_a_devide_b(field_ele_add(p1.y, p2.y), \
+                field_ele_add(p1.x, p2.x))
+            #x = lam**2 + lam + p1.x + p2.x + config.get_a()
+            t1 = field_ele_add(field_ele_g_pow_a(lam, 2), lam)
+            t2 = field_ele_add(field_ele_add(p1.x, p2.x), config.get_a())
+            x = field_ele_add(t1, t2)
+            #y = lam * (p1.x + x) + x + p1.y
+            t1 = field_ele_times(lam, field_ele_add(p1.x, x))
+            t2 = field_ele_add(x, p1.y)
+            y = field_ele_add(t1, t2)
             return Point(x, y)
 
 # 椭圆曲线求 2 倍点 #
@@ -246,18 +377,21 @@ def ECG_double_point(p):
         if ECG_ele_is_zero(p):
             return p
         else:
-            lam = (3 * (p.x**2) + config.get_a()) / (2 * p.y)
-            x = lam**2 - 2 * p.x
-            y = lam * (p.x - x) - p.y
+            t1 = field_ele_add(field_ele_times(3, field_ele_g_pow_a(p.x, 2)), config.get_a())
+            t2 = field_ele_times(2, p.y)
+            lam = field_ele_a_devide_b(t1, t2)
+            x = field_ele_sub(field_ele_g_pow_a(lam, 2), field_ele_times(2, p.x))
+            y = field_ele_sub(field_ele_times(lam, field_ele_sub(p.x, x)), p.y)
             return Point(x, y)
     # F2^m 上的椭圆曲线
     if is_Power_of_two(q):
         if ECG_ele_is_zero(p):
             return p
         else:
-            lam = p.x + (p.y / p.x)
-            x = lam**2 + lam + config.get_a()
-            y = p.x**2 + (lam + 1) * x
+            lam = field_ele_add(p.x, field_ele_a_devide_b(p.y, p.x))
+            x = field_ele_add(field_ele_add(field_ele_g_pow_a(lam, 2), lam), config.get_a())
+            y = field_ele_add(field_ele_g_pow_a(p.x, 2), \
+                field_ele_times(field_ele_add(lam, field_ele_one()), x))
             return Point(x, y)
 
 
@@ -267,29 +401,41 @@ input: 倍数 k 和椭圆曲线点 p
 output: p 的 k 倍点
 '''
 def ECG_k_point(k, p):
-    pass
-'''
+    l = int(math.log2(k)) + 1# - 1
+    point_q = ECG_ele_zero()
+    for i in range(0, l):
+        j = l - 1 - i
+        point_q = ECG_double_point(point_q)
+        if (k & (1 << j)) == (1 << j):
+            point_q = ECG_ele_add(point_q, p)
+    return point_q
+
 # Fp 椭圆曲线测试 #
-config.set_q(23)
-config.set_a(1)
-config.set_b(1)
+#config.set_q(23)
+#config.set_a(1)
+#config.set_b(1)
 ### test ECG_ele_add ###
-print(ECG_ele_add(Point(3, 10), Point(9, 7)))   #(3, 10) + (9, 7) = (17, 20)
+#print(ECG_ele_add(Point(3, 10), Point(9, 7)))   #(3, 10) + (9, 7) = (17, 20)
 ### test ECG_double_point ###
-print(ECG_double_point(Point(3, 10)))   # 2(3, 10) = (7, 12)
+#print(ECG_double_point(Point(3, 10)))   # 2(3, 10) = (7, 12)
+### test ECG_k_point ###
+#print(ECG_k_point(3, Point(3, 10)))
 
 # F2^m 椭圆曲线测试 #
-config.set_q(32)
-config.set_fx('0b100101')
-config.set_a('0b00001')
-config.set_b('0b00001')
+#config.set_q(32)
+#config.set_fx('0b100101')
+#config.set_a('0b00001')
+#config.set_b('0b00001')
 ### test ECG_ele_add ###
 # (01010, 11000) + (01000, 11111) = (11110, 10101)
-print(ECG_ele_add(Point('0b01010', '0b11000'), Point('0b01000', '0b11111')))
+#print(ECG_ele_add(Point('0b01010', '0b11000'), Point('0b01000', '0b11111')))
 ### test ECG_double_point ###
 # [2](01010, 11000) = (01000, 11111)
-print(ECG_double_point(Point('0b01010', '0b11000')))
-'''
+#print(ECG_double_point(Point('0b01010', '0b11000')))
+### test ECG_k_point ###
+# [3](01010, 11000) = (11110, 10101)
+#print(ECG_k_point(3, Point('0b01010', '0b11000')))
+
 # 6.1 密钥对的生成 #
 '''
 input: 有效的椭圆曲线系统参数集合
--- polynomial.py
+++ polynomial.py
@@ -38,6 +38,39 @@ def polynomial_times(a, b):
 ### test polynomial_times ###
 #print(polynomial_times('0b111', '0b11111001'))
 
+# 多项式除法 #
+
+'''
+input: 被除多项式 a 和除多项式 b
+output: a/b
+'''
+
+def polynomial_a_devide_b(a, b):
+    #print("--- 多项式 除法 ---")
+
+    a_bytes = bits_to_bytes(a)
+    a_int = bytes_to_int(a_bytes)
+    a_len = len(a_bytes)
+    b_bytes = bits_to_bytes(b)
+    b_int = bytes_to_int(b_bytes)
+    b_len = len(b_bytes)
+
+    # max result length
+    m = len(a) - 2
+    m_bytes = math.ceil(float(m) / 8.0)
+
+    c = 0
+    i = len(a) - len(b)
+    while i >= 0:
+        a_int = a_int ^ (b_int << i)
+        c += (1 << i)
+        i = len(bytes_to_bits(int_to_bytes(a_int, a_len))) \
+            - len(bytes_to_bits(int_to_bytes(b_int, b_len)))
+    return bytes_to_bits(int_to_bytes(c, m_bytes))
+### test polynomial_a_devide_b ###
+#print(polynomial_a_devide_b('0b1101101001110', '0b111011'))
+#print(polynomial_a_devide_b('0b1011101110', '0b111'))
+
 # 多项式取模 #
 '''
 input: 被除多项式 a 和除多项式 b
